bbe756a0338df25bf0735cbfcd500dfbf19bfca2
TeoTwawki <arawn.annwn@yandex.com>
2020-07-26 16:31:43 -0400

    Verified msgID 166 works like ibm2431 told me, and adjusted its entry in msg.lua while using it in the additional effect global. Adjusted deathres checks to be clearer while adding them to additional effect death proc.

--- scripts/globals/spells/death.lua
+++ scripts/globals/spells/death.lua
@@ -8,13 +8,18 @@ require("scripts/globals/status")
 require("scripts/globals/msg")
 -----------------------------------------
 
-function onMagicCastingCheck(caster, target, spell)
+function onMagicCastingCheck(caster,target,spell)
     spell:setFlag(tpz.magic.spellFlag.IGNORE_SHADOWS)
     return 0
 end
 
-function onSpellCast(caster, target, spell)
-    if target:isUndead() or target:hasStatusEffect(tpz.effect.MAGIC_SHIELD) or math.random(0, 99) < target:getMod(tpz.mod.DEATHRES) then
+function onSpellCast(caster,target,spell)
+    if
+        target:isUndead() or
+        target:hasStatusEffect(tpz.effect.MAGIC_SHIELD) or
+        -- Todo: DeathRes has no place in the resistance functions so far..
+        target:getMod(tpz.mod.DEATHRES) > math.random(100)
+    then
         spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
         return 0
     end
